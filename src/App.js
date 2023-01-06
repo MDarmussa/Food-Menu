988c54fe29792ea98f709b719745546acc5fc8f6
Mohammad <mhamed.mussa@gmail.com>
2023-01-05 18:54:52 -0600

    filtering menu categories completed

--- src/App.js
+++ src/App.js
@@ -10,13 +10,25 @@ function App() {
   //step 1 -- then pass it as a prop by define a variable for <ItemLists itemsData={itemsData}/>
   const [itemsData, setItemsData] = useState(items)
 
+  //2- Filter by category
+  const filterByCategory = (cat) => {
+    //to filter all (All button)
+    if(cat === "All"){
+      setItemsData(items)
+    } else {
+    //if the item is found in the original array, then add newArr in setItemsData()
+      const newArr = items.filter((item) => item.category === cat)
+      setItemsData(newArr)
+    }
+  }
+
   return (
     <div className="color-body font">
       <NavBar />
 
       <Container>
           <Header />
-          <Category />
+          <Category filterByCategory={filterByCategory} />
           <ItemLists itemsData={itemsData} />
       </Container>
 
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 
-function Category() {
+function Category({filterByCategory}) {
+
+  const onFilter = (cat) => {
+    filterByCategory(cat)
+  }
+
   return (
     <Row className='my-2 mb-5'>
      <Col sm='12' className='d-flex justify-content-center'> 
           <div>
-               <button style={{border: "1px solid #b45b35", background: "#B45B35", color:"white"}} className='btn mx-2'>All</button>
-               <button style={{border: "1px solid #b45b35"}} className='btn mx-2'>Breakfast</button>
-               <button style={{border: "1px solid #b45b35"}} className='btn mx-2'>Lunch</button>
-               <button style={{border: "1px solid #b45b35"}} className='btn mx-2'>Dinner</button>
+               <button onClick={() => onFilter('All')} style={{border: "1px solid #b45b35", background: "#B45B35", color:"white"}} className='btn mx-2'>All</button>
+               <button onClick={() => onFilter('Breakfast')} style={{border: "1px solid #b45b35"}} className='btn mx-2'>Breakfast</button>
+               <button onClick={() => onFilter('Lunch')} style={{border: "1px solid #b45b35"}} className='btn mx-2'>Lunch</button>
+               <button onClick={() => onFilter('Dinner')} style={{border: "1px solid #b45b35"}} className='btn mx-2'>Dinner</button>
           </div>
      </Col>
     </Row>
